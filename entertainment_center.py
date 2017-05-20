70bb1b3516da1a8ca9256bc7bd208fda5b320d76
TionChen <wangzuotion@gmail.com>
2017-05-20 22:59:47 +0800

    resubmit

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,34 +1,49 @@
 import media
 import fresh_tomatoes
 
-toy_story = media.Movie("Toy Story",
-                        "A story of a boy and his toys that come to life",
-                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
-#print(toy_story.storyline)
-avater = media.Movie("Avater",
-                        "A marine on an alien planet",
-                        "https://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
-                        "https://www.youtube.com/watch?v=-9ceBgWV8io")
-school_of_rock = media.Movie("School of Rock",
-                        "A story of a boy play rock in campus",
-                        "https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
-                        "https://www.youtube.com/watch?v=3PsUJFEBC74")
-ratatouille = media.Movie("Ratatouille",
-                        "A storyline is waiting",
-                        "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
-                        "https://www.youtube.com/watch?v=c3sBBRxDAqk")
-midnight_in_paris = media.Movie("Midnight in Paris",
-                        "Paris Story",
-                        "https://upload.wikimedia.org/wikipedia/en/9/9f/Midnight_in_Paris_Poster.jpg",
-                        "https://www.youtube.com/watch?v=-9ceBgWV8io")
-wild_child = media.Movie("Wild Child",
-                         "storyline",
-                         "https://upload.wikimedia.org/wikipedia/commons/7/76/Wild_Child.jpg",
-                         "https://www.youtube.com/watch?v=v2oC2vJailQ")
-#print(wild_child.storyline)
-#self.show_trailer()
-#print(media.Movie.valid_ratings)
-movies = [toy_story, avater, school_of_rock, ratatouille, midnight_in_paris, wild_child]
+""" Puttinng some movies' details
+    movies ：a list of instances of Movie
+ """
+toy_story = media.Movie(
+    "Toy Story",
+    "A story of a boy and his toys that come to life",
+    "https://upload.wikimedia.org/wikipedia/en/1/13/"
+    "Toy_Story.jpg",
+    "https://www.youtube.com/watch?v=vwyZH85NQC4")
+# print(toy_story.storyline)
+avater = media.Movie(
+    "Avater",
+    "A marine on an alien planet",
+    "https://upload.wikimedia.org/wikipedia/id/b/b0/"
+    "Avatar-Teaser-Poster.jpg",
+    "https://www.youtube.com/watch?v=-9ceBgWV8io")
+school_of_rock = media.Movie(
+    "School of Rock",
+    "A story of a boy play rock in campus",
+    "https://upload.wikimedia.org/wikipedia/en/1/11/"
+    "School_of_Rock_Poster.jpg",
+    "https://www.youtube.com/watch?v=3PsUJFEBC74")
+ratatouille = media.Movie(
+    "Ratatouille",
+    "A storyline is waiting",
+    "https://upload.wikimedia.org/wikipedia/en/5/50/"
+    "RatatouillePoster.jpg",
+    "https://www.youtube.com/watch?v=c3sBBRxDAqk")
+midnight_in_paris = media.Movie(
+    "Midnight in Paris",
+    "Paris Story",
+    "https://upload.wikimedia.org/wikipedia/en/9/9f/"
+    "Midnight_in_Paris_Poster.jpg",
+    "https://www.youtube.com/watch?v=-9ceBgWV8io")
+wild_child = media.Movie(
+    "Wild Child",
+    "storyline",
+    "https://upload.wikimedia.org/wikipedia/commons/7/76"
+    "/Wild_Child.jpg",
+    "https://www.youtube.com/watch?v=v2oC2vJailQ")
+# print(wild_child.storyline)
+# self.show_trailer()
+# print(media.Movie.valid_ratings)
+movies = [toy_story, avater, school_of_rock, ratatouille, \]
+[midnight_in_paris, wild_child]
 fresh_tomatoes.open_movies_page(movies)
-
--- media.py
+++ media.py
@@ -1,12 +1,25 @@
 import webbrowser
 class Movie():
-    """here is my movie class"""
+    """here is my movie class
+	args:
+	valid_ratings(list): include Movie Rating System's different levels
+	"""
     valid_ratings = {"G", "PG", "PG-13", "R"}
     
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+	"""init method
+	args:
+	movie_title(str): a movie's title
+	movie_storyline(str): summary about the movie
+	poster_image(str): an url link to special movie poster pic
+	trailer_youtube(str): an url link to movie's youtube play 
+	"""
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
     def show_trailer(self):
+	"""
+	import webbrowser to open movie's youtube url link
+	"""
         webbrowser.open(self.trailer_youtube_url)
